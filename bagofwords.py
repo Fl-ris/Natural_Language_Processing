693b3c1c6ab6b53851485b45abd43d456b6fcddd
MirteDraaijer <mirtedraaijer@outlook.com>
2025-12-08 15:19:04 +0100

    multi-hot en tfidf toegevoegd,schrijven naar .bow toegevoegd en docstrings toegevoegd

--- bagofwords.py
+++ bagofwords.py
@@ -1,23 +1,3 @@
-# Stel dat een leesbaar *.txt tekstbestand gegeven is met daarin een corpus bestaande uit een reeks documenten. 
-# Elk document staat op één regel (d.w.z. een document kan uit meerdere zinnen bestaan, maar bevat geen regeleinden). 
-# Schrijf een python-script bagofwords dat kan worden aangeroepen vanaf de command line en dat elk document omzet in een 
-# reeks getalwaarden. De gebruiker dient te kunnen aangeven of een multi-hot, telling/frequentie, of TF-IDF encoding gebruikt 
-# dient te worden. Het resultaat dient te worden opgeslagen in een *.bow databestand met de bag-of-words representaties. Kies 
-# zelf een geschikte bestandsindeling.
-
-# Het dient ook mogelijk te zijn om meerdere leesbare *.txt tekstbestanden mee te geven. Die dienen dan allemaal gezamenlijk 
-# te worden omgezet in hun eigen *.bow databestanden. Daarbij dient voor elk bestand dezelfde tokenisatie gebruikt te worden 
-# en ook de TF-IDF berekening dient gebaseerd te worden op alle documenten tezamen (d.w.z. maak één byte-pair encoding op basis 
-# van alle informatie in alle documenten gezamenlijk, en tel de frequenties van documenten die een zeker token bevatten over alle 
-# documenten heen).
-
-# Geef bondige documentatie van het gebruik weer als de gebruiker je script aanroept met bagofwords --help of bagofwords -h.
-
-# Breng klassen en functies die van algemeen belang zijn onder in een aparte module nlp.py die gedeeld is over alle lessen, 
-# en importeer deze in je script. Je script kan daarnaast eigen klassen en functies definiëren voor eigen gebruik.
-
-# Eerste test met voorbeeld: https://www.datacamp.com/tutorial/python-bag-of-words-model?dc_referrer=https%3A%2F%2Fwww.google.com%2F 
-
 from tokenize_mirte import *
 import math
 import argparse
@@ -45,7 +25,59 @@ def count_encoding(doc, vocab):
     return vector
 
 
+def multi_hot(doc, vocab):
+    """
+    Encode het opgegeven bestand met multi-hot encoding
+    :param doc: een .txt bestand met daarin op elke regel een bestand.
+    :param vocab: een lijst met alle woorden in het bestand.
+    :return vector: een vector met daarin de encoding.
+    """
+    vector = [0] * len(vocab)
+    for token in doc:
+        if token in vocab:
+            vector[vocab[token]] = 1
+    return vector
+
+
+def tf_idf_vector(doc, vocab, idf_scores):
+    """
+    Encode het opgegeven bestand met TF-IDF
+    :param doc: een .txt bestand met daarin op elke regel een bestand.
+    :param vocab: een lijst met alle woorden in het bestand.
+    :param idf_scores: de IDF-score
+    :return tfidf_vector: een vector met daarin de encoding.
+    """
+    tf_vector = multi_hot(doc, vocab)
+    tfidf_vector = [round(tf * idf_scores[i], 3) for i, tf in enumerate(tf_vector)]
+    return tfidf_vector
+
+
+def compute_idf(docs, vocab):
+    """
+    Berekent de IDF voor elk bestand in het bestand
+    :param docs: een .txt bestand met daarin op elke regel een bestand.
+    :param vocab: een lijst met alle woorden in het bestand.
+    :return idf:
+    """
+    N = len(docs)
+    df = [0] * len(vocab)
+
+    for doc in docs:
+        seen = set()
+        for token in doc:
+            if token in vocab and token not in seen:
+                df[vocab[token]] += 1
+                seen.add(token)
+    idf = [math.log((N + 1) / (df_i + 1)) + 1 for df_i in df]
+    return idf
+
+
 def apply_bpe(doc, merges):
+    """
+    BPE uitvoeren op het gegeven document.
+    :param doc: een document.
+    :param merges: de merges in van tokens in het document.
+    """
     tokens = doc[:]
     flat_tokens = []
     for merge in merges:
@@ -55,15 +87,39 @@ def apply_bpe(doc, merges):
     return flat_tokens
 
 
+def write_to_bow(vocabulary, vectors, path):
+    """
+    Schrijft de gegenereerde bag of words representatie weg naar .bow bestand.
+    :param vocabulary: de gebruikte vocabulary.
+    :param vectors: de vectoren met daarin de bag of words representatie.
+    :param path: het pad wanneer het bestand wordt geschreven.
+    """
+    with open(path, "w", encoding = "utf-8") as f:
+        f.write("Gebruikte vocabulary:\n")
+        f.write(f"{vocabulary}\n\n")
+        f.write("Bag of words representaties:\n")
+        for vector in vectors:
+            f.write(f"{vector}\n")
+
+
 def main():
-    corpus = ["Python is amazing and fun.", "Python is not just but also powerful", "Learning python is fun!"]
+    parser = argparse.ArgumentParser(description = "Bag of words encoder")
+    parser.add_argument("filepath", nargs = "+", help = "Path(s) to corpus text file")
+    parser.add_argument("--encoding", type = str, choices = ["multi-hot", "count", "tfidf"],
+                        default = "count", help = "Type of encoding")
+    parser.add_argument("--minfreq", type = int,
+                        default = 2, help = "Minimum frequency of tokens in the text")
+    args = parser.parse_args()
+    
+    all_documents = []
+    for path in args.filepath:
+        with open(path, "r", encoding = "utf-8") as f:
+            all_documents.append([pre_process(line) for line in f])
 
-    documents = [pre_process(sentence) for sentence in corpus]
-
-    flat_tokens = [token for doc in documents for token in doc]
+    flat_tokens = [token for doc_list in all_documents for doc in doc_list for token in doc]
 
     merges = []
-    min_frequency = 2
+    min_frequency = args.minfreq
 
     for _ in range(9999):
         token_set = tokenizer(flat_tokens)
@@ -76,15 +132,34 @@ def main():
     vocab_count, vocabulary = get_vocabulary(flat_tokens)
     vocab_to_idx = {token: i for i, token in enumerate(vocabulary)}
 
-    bpe_documents = [apply_bpe(doc, merges) for doc in documents]
+    bpe_per_file = []
+    for docs in all_documents:
+        bpe_per_file.append([apply_bpe(doc, merges) for doc in docs])
 
-    bow_vectors = [count_encoding(doc, vocab_to_idx) for doc in bpe_documents]
+    if args.encoding == "tfidf":
+        idf_scores = compute_idf([doc for file_docs in bpe_per_file for doc in file_docs], vocab_to_idx)
+    else:
+        idf_scores = None
+    
+    for path, bpe_docs in zip(args.filepath, bpe_per_file):
+        vectors = []
+        for doc in bpe_docs:
+            if args.encoding == "multi-hot":
+                vectors.append(multi_hot(doc, vocab_to_idx))
+            elif args.encoding == "count":
+                vectors.append(count_encoding(doc, vocab_to_idx))
+            elif args.encoding == "tfidf":
+                vectors.append(tf_idf_vector(doc, vocab_to_idx, idf_scores))
+        
+        out_path = path.replace(".txt", ".bow")
+        write_to_bow(vocabulary, vectors, out_path)
+        print(f"Bag of words met het bestand: {path}")
+        print(f"Vocabulary: {vocabulary}\n")
+        print(f"Bag of words vectors ({args.encoding}):")
+        for vec in vectors:
+            print(vec)
+        print(f"\nBOW succesvol geschreven naar {out_path}")
 
-    print("Vocabulary: ", vocabulary)
-    print("Bag of words vectors:")
-    for vec in bow_vectors:
-        print(vec)
 
 if __name__ == "__main__":
     main()
-    
